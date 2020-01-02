30575e325d617014c2e9f234f799c2a2950498c6
mattbechtel1 <bechma02@gmail.com>
2020-01-02 15:28:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+function Breakfast(food, drink) {
+    this.food = food;
+    this.drink = drink;
+}
+
+function Lunch(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+}
+
+function Dinner(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+}
